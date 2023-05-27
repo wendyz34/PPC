d19881a3303e42178efb262e218ae8c491e8aae8
wendy <wendyz34@nycstudents.net>
2023-05-27 17:59:28 -0400

    Initial commit

---
+++ src/ProductionPossibilityCurve.java
@@ -0,0 +1,212 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.awt.event.MouseEvent;
+
+public class ProductionPossibilityCurve extends JPanel{
+
+    private List<Point> points;
+    private String xAxisLabel;
+    private String yAxisLabel;
+    private static int con;
+    private static int cap;
+    private Point dragPoint;
+
+    private JLabel coordinatesLabel;
+
+    public ProductionPossibilityCurve(String xAxisLabel, String yAxisLabel) {
+        points = new ArrayList<>();
+        this.xAxisLabel = xAxisLabel;
+        this.yAxisLabel = yAxisLabel;
+        coordinatesLabel = new JLabel("");
+        add(coordinatesLabel);
+        // Set the layout manager for the panel
+        setLayout(new BorderLayout());
+        //Click Points
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Point clickedPoint = getClickedPoint(e.getX(), e.getY());
+                if (clickedPoint != null) {
+                    handlePointClick(clickedPoint);
+                }
+            }
+        });
+
+        // Initialize the coordinates label
+        coordinatesLabel = new JLabel("");
+        add(coordinatesLabel, BorderLayout.SOUTH);
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    dragPoint = getClickedPoint(e.getX(), e.getY());
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    dragPoint = null;
+                }
+        });
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                   if (dragPoint != null) {
+                    int width = getWidth();
+                    int height = getHeight();
+                    int xCenter = width / 2;
+                    int yCenter = height / 2;
+
+                    dragPoint.x = e.getX() - xCenter;
+                    dragPoint.y = yCenter - e.getY();
+
+                    // Update the coordinates label
+                    coordinatesLabel.setText("Coordinates: (" + dragPoint.x + ", " + dragPoint.y + ")");
+                    //Calls paintComponent so the coordinates update and reflect.
+                    repaint();
+                }
+            }
+        });
+    }
+    //Adds points to list
+    public void addPoint(int x, int y) {
+        points.add(new Point(x, y));
+    }
+    private Point getClickedPoint(int x, int y) {
+        int width = getWidth();
+        int height = getHeight();
+        int xCenter = width / 2;
+        int yCenter = height / 2;
+
+        for (Point point : points) {
+            int pointX = xCenter + point.x;
+            int pointY = yCenter - point.y;
+
+            if (Math.abs(pointX - x) <= 5 && Math.abs(pointY - y) <= 5) {
+                return point;
+            }
+        }
+
+        return null;
+    }
+
+
+    private void handlePointClick(Point clickedPoint) {
+        int x = clickedPoint.x;
+        int y = clickedPoint.y;
+        int width = getWidth();
+        int height = getHeight();
+        int xCenter = width / 2;
+        int yCenter = height / 2;
+        double radius = Math.sqrt(x*x+y*y);
+        String message = "";
+        if ((x - xCenter) * (x - xCenter) +
+                (y - yCenter) * (y - yCenter) > radius * radius) {
+            message = "Below the curve, might be in a recession";
+        }else if ((x - xCenter) * (x - xCenter) +
+                (y - yCenter) * (y - yCenter) < radius * radius){
+            message = "Above the curve, technological advancement";
+        }else{
+            message = "On the curve, normal production";
+        }
+        if((x==cap)&&(y==0)||(y==con)&&(x==0)){
+            message = "On the curve, normal production";
+        }
+
+        JOptionPane.showMessageDialog(this, "Clicked point: (" + x + ", " + y + ") " + message);
+    
+    }
+
+
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        super.paintComponent(g);
+
+        int width = getWidth();
+        int height = getHeight();
+        int xCenter = width / 2;
+        int yCenter = height / 2;
+
+        // Draw the vertical line
+        g.setColor(Color.GRAY);
+        g.drawLine(xCenter, 0, xCenter, height / 2);
+
+        // Draw the horizontal line
+        g.drawLine(xCenter, height / 2, width, height / 2);
+
+        // Draw the x and y axis labels
+        g.setColor(Color.BLACK);
+        g.drawString(xAxisLabel, width - 70, height / 2 - 10);
+        g.drawString(yAxisLabel, xCenter + 10, 20);
+
+        // Plot the user-provided points
+        g.setColor(Color.RED);
+        for (Point point : points) {
+            int x = xCenter + point.x;
+            int y = yCenter - point.y;
+            g.fillOval(x - 2, y - 2, 10, 10);
+        }
+
+        // Draw the arc
+        g.setColor(Color.BLUE);
+        int arcStartX = xCenter - cap;
+        int arcStartY = yCenter - con;
+        int arcWidth = cap * 2;
+        int arcHeight = con * 2;
+
+        g.drawArc(arcStartX, arcStartY, arcWidth, arcHeight, 0, 90);
+    }
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Production Possibility Curve");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(550,550);
+        Scanner scanner = new Scanner(System.in);
+        //Label axis
+        //coordinates only show within 250?
+        String xAxisLabel= JOptionPane.showInputDialog("Label x:");
+        String yAxisLabel= JOptionPane.showInputDialog("Label y:");
+        ProductionPossibilityCurve graphPanel = new ProductionPossibilityCurve(xAxisLabel, yAxisLabel);
+        //Get slope
+        //max production only show within 500 or 250?
+        String slope = JOptionPane.showInputDialog("During a normal economic time what's the max production of consumer goods?");
+        con = Integer.parseInt(slope);
+        String s = JOptionPane.showInputDialog("Max production of consumer goods?");
+        cap = Integer.parseInt(s);
+        //get coordinates
+        while (true) {
+            /*System.out.println("Enter coordinates (x, y) separated by spaces (or press enter to finish): ");
+            String input = scanner.nextLine();*/
+            String input = JOptionPane.showInputDialog("Enter coordinates (x, y) separated by spaces (or press enter to finish): ");
+            if (input.isEmpty()) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+            if (parts.length != 2) {
+                System.out.println("Invalid input! Please enter coordinates in the format 'x y'.");
+                continue;
+            }
+            try {
+                int x = Integer.parseInt(parts[0]);
+                int y = Integer.parseInt(parts[1]);
+                graphPanel.addPoint(x, y);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input! Please enter integers for the coordinates.");
+                continue;
+            }
+        }
+
+        frame.add(graphPanel);
+
+        frame.setVisible(true);
+        //scanner.close();
+    }
+}
